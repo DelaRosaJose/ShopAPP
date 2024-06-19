7b937dce73a18bd9c6d341bb87647d74e03b6cdf
DelaRosa_Jose <josealberto3053@gmail.com>
2024-06-18 20:41:31 -0400

    Add project files.

---
+++ ShopAPP.DAL/Interfaces/ICustomersDb.cs
@@ -0,0 +1,13 @@
+﻿using ShopAPP.DAL.Models.Customers;
+
+namespace ShopAPP.DAL.Interfaces
+{
+    public interface ICustomersDb
+    {
+        List<CustomersModel> GetCategories();
+        CustomersModel GetCategoriesById(int id);
+        void SaveCategories(CustomersAddModel categoriesAdd);
+        void UpdateCategories(CustomersUpdateModel categoriesMod);
+        void RemoveCategories(CustomersRemoveModel categoriesRem);
+    }
+}
